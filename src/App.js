15412488af2e8387337a5819bb8863ce71c0bbf8
Tristan Hall <tristan.hall@procensus.com>
2022-01-24 13:24:54 +0000

    updated react-scripts version to work with tailwind.css, updated parsers and added react babel parsers' '

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import axios from 'axios'
+/* eslint-disable semi */
+import React from 'react';
+import axios from 'axios';
 
 function App() {
   React.useEffect(() => {
     const getData = async () => {
-      const res = await axios.get('/api/dogs')
-      console.log(res.data)
-    }
-    getData()
-  })
+      const res = await axios.get('/api/dogs');
+      console.log(res.data);
+    };
+    getData();
+  });
 
-  return <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
+  return <h1 className="text-3xl font-bold underline"> Hello world! </h1>;
 }
 
-export default App
+export default App;
