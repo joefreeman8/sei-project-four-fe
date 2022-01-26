4227c6375e270f51a189144ac98ed563bf41ba49
Mike Salter <mikey.l.salter@gmail.com>
2022-01-26 14:40:14 +0000

    dog show no login required

--- src/components/dogs/DogShow.js
+++ src/components/dogs/DogShow.js
@@ -21,14 +21,23 @@ function DogShow() {
   const [isNew, setIsNew] = React.useState(false)
   const dogImages = []
 
-
-
-  const favoriteObject = {
-    dog: dogId,
-    owner: getUserId().toString(),
+  let favoriteObject = {
+    dog: '',
+    owner: '',
   }
 
-  
+  const userCheck = () => {
+    if (getUserId()) {
+      favoriteObject = {
+        dog: dogId,
+        owner: getUserId().toString(),
+      }
+      return getUserId().toString()
+    } else return getUserId()
+  }
+
+
+
 
   React.useEffect(() => {
     const getData = async () => {
@@ -62,19 +71,19 @@ function DogShow() {
         console.log(err)
       }
     }
-    
+
   }
 
 
   const createImageArray = (dog) => {
     dogImages.push(dog.imageOne) &&
-    (!dog.imageTwo || dogImages.push(dog.imageTwo)) &&
-    (!dog.imageThree || dogImages.push(dog.imageThree))
+      (!dog.imageTwo || dogImages.push(dog.imageTwo)) &&
+      (!dog.imageThree || dogImages.push(dog.imageThree))
     return
   }
 
   const favoriteCheck = (favorites) => {
-    favorites.map(favorite =>{
+    favorites.map(favorite => {
       favorite.owner.id === getUserId()
       setFavoriteId(favorite.id.toString())
       setIsFavorited(true)
@@ -87,12 +96,13 @@ function DogShow() {
     const dogAdded = new Date(dog.dateAdded)
     const dogAddedDate = Date.parse(dogAdded)
     console.log(nowSeconds - dogAddedDate)
-    if (nowSeconds - dogAddedDate < 2068935000 ) {
+    if (nowSeconds - dogAddedDate < 2068935000) {
       setIsNew(true)
     }
   }
 
   const carouselProps = { dogImages, 'isNew': isNew }
+  userCheck()
 
 
   return (
@@ -104,9 +114,9 @@ function DogShow() {
         <p className="text-sm">Find out how rehoming from us works and how to get started finding your perfect match.</p>
         <button className="bg-pawhub-yellow hover:bg-pawhub-yellow/50 text-pawhub-grey font-bold py-2 px-4 m-3 rounded">How rehoming works &gt;</button>
       </div>
-      
+
       {dog &&
-      
+
         <>
           {createImageArray(dog)}
           <div className="bg-pawhub-purple">
@@ -120,9 +130,9 @@ function DogShow() {
                   <p className="text-base">I&apos;m looking for a home...</p>
                 </div>
                 <a onClick={handleFavorite}><img src={!isFavorited ? emptyHeart : fullHeart} className="w-10 h-10" /></a>
-                
+
               </div>
-              
+
               <Carousel {...carouselProps} />
               <h1 className="gooddog-font text-3xl">About {dog.name}</h1>
               <hr />
@@ -140,22 +150,22 @@ function DogShow() {
               <div className="px-2">
                 <p>{dog.homeDetails}</p>
               </div>
-              {!dog.canLiveWithCats & !dog.canLiveWithDogs & ! dog.canLiveWithKids  ? 
+              {!dog.canLiveWithCats & !dog.canLiveWithDogs & !dog.canLiveWithKids ?
                 <></> :
                 <><h1 className="gooddog-font text-2xl">Important Information</h1>
                   <div className="px-2">
                     {dog.canLiveWithDogs &&
-                    <p><img src={dogPaw} className="w-7 inline"/> May live with dogs</p>
+                      <p><img src={dogPaw} className="w-7 inline" /> May live with dogs</p>
                     }
                     {dog.canLiveWithCats &&
-                    <p><img src={cat} className="w-7 inline"/> May live with cats</p>
+                      <p><img src={cat} className="w-7 inline" /> May live with cats</p>
                     }
                     {dog.canLiveWithKids &&
-                    <p><img src={baby} className="w-7 inline"/> May live with children</p>
+                      <p><img src={baby} className="w-7 inline" /> May live with children</p>
                     }
                   </div></>
               }
-              
+
               <div className="pt-5">
                 <p><strong>Donate today, to help us continue our work providing life-changing care and finding forever homes for thousands of dogs a year. We&apos;re so grateful for your support.</strong></p>
               </div>
@@ -163,14 +173,14 @@ function DogShow() {
                 <a href="/rehomingform">
                   <button className="bg-pawhub-purple hover:bg-pawhub-purple/50 text-white font-bold py-2 px-4 m-3 rounded">Rehoming me starts here</button>
                 </a>
-                
+
                 <button className="bg-pawhub-yellow hover:bg-pawhub-yellow/50 text-pawhub-grey font-bold py-2 px-4 m-3 rounded">How rehoming works &gt;</button>
               </div>
-              
+
 
             </div>
           </div>
-        
+
 
         </>
       }
