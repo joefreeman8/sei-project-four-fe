e523c210a1432f7af40afc36d9a1853086e111ef
Joe Freeman <joefreeman8@gmail.com>
2022-01-26 09:29:02 +0000

    start of who we are page

--- src/components/common/AboutUs/WhoWeAre.js
+++ src/components/common/AboutUs/WhoWeAre.js
@@ -1,6 +1,23 @@
+import React from 'react'
+
+import twopawbanner from '../../../assets/twopawbanner.png'
+
 function WhoWeAre() {
   return (
-    <h1>Who we are page</h1>
+    <>
+      <div className="background-image-container">
+        <h1 className="image-text kessel-font" id="title">Who We Are</h1>
+        <p className="image-text text-lg mt-10" id="whoweare-tagline">
+          Meet the team</p>
+        <img src={twopawbanner} className="background-image" />
+      </div>
+      <div className="bg-pawhub-purple">
+        <p className="text-white text-base py-4 pl-10"><a href="/" className="hover:underline">Home</a> &gt; <a href="/aboutus" className="hover:underline">About Us</a></p>
+      </div>
+      <div className="bg-pawhub-yellow flex justify-center items-center p-5">
+
+      </div>
+    </>
   )
 }
 
