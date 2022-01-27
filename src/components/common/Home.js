f1b33ae47f0eba218b36aecf1ca1287a26822e4e
Mike Salter <mikey.l.salter@gmail.com>
2022-01-27 15:25:52 +0000

    homepage minor edits

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -65,42 +65,40 @@ function Home() {
             <div className="bg-pawhub-yellow h-auto w-full lg:w-1/3 text-xl text-center p-5 lg:flex lg:flex-col lg:items-center lg:justify-center">
               <p>I spend all day looking out this window, wondering if I&apos;ll ever be loved again</p>
               <Link to="/dogs">
-                <button className="bg-pawhub-grey rounded-md text-white text-2xl w-auto h-auto p-7 mt-5 hover:bg-white hover:text-pawhub-grey hover:border-2 hover:border-pawhub-grey">
+                <button className="bg-pawhub-grey rounded-md text-white text-2xl w-auto h-auto p-7 mt-5 hover:bg-white hover:text-pawhub-grey border-2 border-pawhub-grey">
                   Dogs for adoption
                 </button>
               </Link>
             </div>
           </div>
-
-          {/* Donate portion */}
-          <div className="flex flex-col items-center bg-pawhub-stone">
+          <div className="flex flex-col items-center bg-pawhub-stone pb-5">
             <h3 className="kessel-font m-5 text-6xl">Donate Today</h3>
             <p>Every penny will help a dog in need</p>
             <Carousel />
 
-            <Link to="/donation">
-              <button className="block mx-auto bg-pawhub-grey rounded-md text-white h-16 w-64 mt-5 hover:bg-white hover:text-pawhub-grey hover:border-2 hover:border-pawhub-grey">
+            <Link to="/donation" className="pb-10">
+              <button className="block mx-auto bg-pawhub-grey rounded-md text-white h-16 w-64 mt-5 hover:bg-white hover:text-pawhub-grey border-2 border-pawhub-grey">
                 Donate
               </button>
             </Link>
             <div className="flex flex-col items-center bg-white pb-10">
               <h3 className="kessel-font text-6xl m-5">Adopt A Dog</h3>
-              <p>These are just 10 of the <span className='font-bold'>{dogs.length}</span> dogs that need a forver home</p>
+              <p>These are just a few of the <span className='font-bold'>{dogs.length}</span> dogs that need a forver home</p>
               <div className="flex flex-row">
 
               </div>
-              <div className="w-full flex object-contain flex-row">
-                <button><AiOutlineLeft onClick={moveDogs} className='text-3xl inset-y-1/2 text-pawhub-purple cursor-pointer' data-id='left' /></button>
+              <div className="w-full flex flex-col md:flex-row">
+                <button className='invisible md:visible'><AiOutlineLeft onClick={moveDogs} className='text-3xl inset-y-1/2 text-pawhub-purple cursor-pointer' data-id='left' /></button>
 
                 {dogsToShow.map(dog =>
                   <DogHomeCard key={dog.id} {...dog} />
                 )}
 
-                <button> <AiOutlineRight onClick={moveDogs} className='text-3xl inset-y-1/2 text-pawhub-purple cursor-pointer' data-id='right' /> </button>
+                <button className="invisible md:visible"> <AiOutlineRight onClick={moveDogs} className='text-3xl inset-y-1/2 text-pawhub-purple cursor-pointer' data-id='right' /> </button>
 
               </div>
               <Link to="/dogs">
-                <button className="block mx-auto bg-pawhub-grey hover:bg-white rounded-md text-white hover:text-pawhub-grey hover:border-2 hover:border-pawhub-grey h-16 w-64 mt-5">
+                <button className="block mx-auto bg-pawhub-grey hover:bg-white rounded-md text-white hover:text-pawhub-grey border-2 border-pawhub-grey h-16 w-64 mt-5">
                   See our Dogs
                 </button>
               </Link>
