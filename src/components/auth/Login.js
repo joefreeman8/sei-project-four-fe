1d5887b3076395d81b2686200b7b6899455b399f
Mike Salter <mikey.l.salter@gmail.com>
2022-01-27 18:09:32 +0000

    login fixes

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -34,15 +34,12 @@ function Login() {
   }
 
   return (
-    <div className="container bg-donationTile bg-repeat-round h-full min-w-full flex flex-col justify-center items-start">
-      {/* <div className="mx-auto">
-        <h1 className="text-6xl text-pawhub-grey font-bold align-center p-8">Login</h1>
-      </div> */}
+    <div className="container bg-donationTile bg-repeat-round h-full min-w-full flex flex-col justify-center items-center">
       <div className="login-background h-96 w-full bg-center bg-cover">
 
       </div>
-      <form className="bg-white w-2/3 h-1/2 pb-10 mb-20 ml-16 -mt-32 flex flex-col shadow" onSubmit={handleSubmit}>
-        <h2 className="text-2xl p-5">Login</h2>
+      <form className="bg-white w-4/5 md:w-2/3 h-1/2 px-5 pb-10 mb-20 mx-auto -mt-32 flex flex-col items-center shadow" onSubmit={handleSubmit}>
+        <h2 className="text-2xl pt-5">Login</h2>
         <hr className="mx-5 mb-5"></hr>
         {isError && <div>
           <div className="border border-red-400 rounded-b bg-red-100 px-4 py-3 w-4/5 mt-5 mx-auto text-red-700">
@@ -53,20 +50,18 @@ function Login() {
             Username
         </label>
         <input
-          className="block shadow rounded border-2 h-8 w-1/2 py-2 px-3 mx-5 text-gray-700 leading-tight focus:outline-none focus:border-pawhub-yellow mb-5" id="username" type="text" name="username" placeholder="Enter your username" onChange={handleChange}>
+          className="block shadow rounded border-2 h-16 w-full py-2 px-3 mx-5 text-gray-700 leading-tight focus:outline-none focus:border-pawhub-yellow mb-5 rounded-lg" id="username" type="text" name="username" placeholder="Enter your username" onChange={handleChange}>
         </input>
         <label className="block mx-5 mb-2 text-pawhub-grey text-sm font-bold mb-2" htmlFor="password">
             Password
         </label>
-        <input className="block shadow rounded border-2 h-8 w-1/2 py-2 px-3 mx-5 text-gray-700 leading-tight focus:outline-none focus:border-pawhub-yellow mb-5" id="passowrd" type="password" name="password" placeholder="Password" onChange={handleChange}>
+        <input className="block shadow rounded border-2 h-16 w-full py-2 px-3 mx-5 text-gray-700 leading-tight focus:outline-none focus:border-pawhub-yellow mb-8 rounded-lg" id="passowrd" type="password" name="password" placeholder="Password" onChange={handleChange}>
         </input>
-        <div className="h-14 flex justify-end">
-          <button className="rounded-md w-1/6 mr-32 bg-cyan-600 text-white font-bold" type="submit">
+        <button className="rounded-md w-full h-12 mb-8 bg-pawhub-yellow text-black text-xl font-bold" type="submit">
               Login 
-          </button>
-        </div>
+        </button>
         <div className="mx-5 text-blue-700">
-          <Link to="/register" >Signup</Link>
+          <Link className='text-pawhub-purple' to="/register" >Don&apos;t have an account? Signup</Link>
         </div>
       </form>
     </div>
